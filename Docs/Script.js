13b764a8c889be97d532403e44ffb2e6a3e7f9a8
Mahdie Setoude <hedistdh@gmail.com>
2025-07-18 14:41:37 +0330

    Create Script.js

---
+++ Docs/Script.js
@@ -0,0 +1,15 @@
+let totalCalories = 0;
+
+function addFood() {
+  const food = document.getElementById("foodInput").value;
+  const calories = parseInt(document.getElementById("calorieInput").value);
+
+  if (food && !isNaN(calories)) {
+    const li = document.createElement("li");
+    li.textContent = `${food}: ${calories} کالری`;
+    document.getElementById("foodList").appendChild(li);
+
+    totalCalories += calories;
+    document.getElementById("totalCalories").textContent = `کالری کل: ${totalCalories}`;
+  }
+}
